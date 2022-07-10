c67e59181506e105f0ad5eb7ff058b42c40ded1e
Paul Sobande <paotec22@gmail.com>
2022-07-11 00:29:33 +0100

    function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/* Code Credit : Ugobo Thankgod & Paul Sobande */
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ * Return: number of chars printed.
+ */
+
